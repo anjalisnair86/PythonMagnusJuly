4401850a989648a945c995e01782986a6e1fa1d8
anjalisnair86 <anjali.snair86@gmail.com>
2023-08-04 12:04:19 -0400

    day5_27_july

---
+++ if else example.py
@@ -0,0 +1,15 @@
+###bmi calculator###
+
+name="patrik"
+weight_kg=70
+height_m=2
+
+bmi=bmi=weight_kg/(height_m**2)
+print("bmi: ")
+print(bmi)
+if bmi<25:
+    print(name)
+    print("is not over weight")
+else:
+    print(name)
+    print("is over weight")
