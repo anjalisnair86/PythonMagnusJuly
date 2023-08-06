7c155f6249a545737a862f98a56c7bc70f8fa470
anjalisnair86 <anjali.snair86@gmail.com>
2023-08-06 16:28:08 -0400

    test

--- additional work/conditional handson.py
+++ additional work/conditional handson.py
@@ -1,6 +1,15 @@
-response_name = input("enter name: ")
-response_marks = input("enter mark: ")
-if int(response_marks) > 50:
-    print("pass")
+####Question1####
+#response_name = input("enter name: ")
+#response_marks = input("enter mark: ")
+#if int(response_marks) > 50:
+    #print("pass")
+#else:
+    #print("fail")
+######question 2#####
+response_length=input("enter length: ")
+response_width=input("enter width: ")
+if int(response_length==response_width):
+     print("square")
 else:
-    print("fail")
+      print("rectangle")
+
