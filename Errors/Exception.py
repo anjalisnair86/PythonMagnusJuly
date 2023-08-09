4e773fd813a5fc8ffe2014c688576ef21ab4665d
anjalisnair86 <anjali.snair86@gmail.com>
2023-08-09 11:33:43 -0400

    test

---
+++ Errors/Exception.py
@@ -0,0 +1,12 @@
+class Sample:
+    def m1(self):
+        a=10
+        b=0
+        try:
+             print(a/b)
+        except Exception as e:
+             print(e)
+
+obj1 = Sample()
+obj1.m1()
+print("end of the program")
