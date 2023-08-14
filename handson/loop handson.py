b9bfd01cbf671bfa507608f84a8f797bec489d18
anjalisnair86 <anjali.snair86@gmail.com>
2023-08-14 17:09:09 -0400

    test

--- handson/loop handson.py
+++ handson/loop handson.py
@@ -2,6 +2,27 @@
 m = int(input("enter a number: "))
 n = int(input("enter a number: "))
 k=[]
-for i in range(m,n):
+for i in range(m,n+1):
     k.append(i)
 print(k)
+####Question 2
+
+n=(int(input("enter a number: ")))
+k=[]
+for i in range(1,n+1):
+    k.append(i)
+average=sum(k)/len(k)
+print(average)
+####question 5
+
+n = (int(input("enter a number: ")))
+k = []
+for i in range(n+1):
+     k.append(i)
+print(k)
+print(sum(k))
+
+
+
+
+
