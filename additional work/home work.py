663029e405cb143b2d3b284a9d0afa8090445f83
anjalisnair86 <anjali.snair86@gmail.com>
2023-08-16 11:37:03 -0400

    test

--- additional work/home work.py
+++ additional work/home work.py
@@ -1,3 +1,26 @@
+class Sample:
+    def m1(self):
+        a = 10
+        b = 0
+        try:
+            print(a/b)
+        except ZeroDivisionError as e:
+            print(e)
+        finally:
+            print("Zero division done")
+
+    def m2(self):
+        c=10
+        try:
+            print(d)
+        except NameError as e:
+            print(e)
+        finally:
+            print("name error done")
+
+obj = Sample()
+obj.m1()
+obj.m2()
 
 
 
